9e83ff2b9d42449ae019dc27ac85450e9a1a0bda
Pedro Farias <phjfg@cesar.org.br>
2022-07-17 01:45:44 +0000

    fix: update event imports

--- src/domain/customer/event/handler/send-notification-when-customer-is-created.handler.ts
+++ src/domain/customer/event/handler/send-notification-when-customer-is-created.handler.ts
@@ -1,4 +1,4 @@
-import EventHandlerInterface from "../../../@shared/event/@shared/event-handler.interface";
+import EventHandlerInterface from "../../../@shared/event/event-handler.interface";
 import CustomerCreatedEvent from "../customer-created.event";
 
 export default class SendNotificationWhenCustomerIsCreated implements EventHandlerInterface<CustomerCreatedEvent>{
